dcadbfddd1ce185d4f62a234fdd0c0a90737a010
annisa-rachma <saa.annisa15@gmail.com>
2025-02-14 06:22:47 +0700

    add grid

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,147 @@ function App() {
 
   return (
     <>
-      <div className=''>
-        test
+      {/**1 */}
+      <div className='border-1 rounded-tl-sm'>
+        1
+      </div>
+      <div className='border-1'>
+        2
+      </div>
+      <div className='border-1'>
+        3
+      </div>
+      <div className='border-1'>
+        4
+      </div>
+      <div className='border-1'>
+        5
+      </div>
+      <div className='border-1'>
+        6
+      </div>
+      <div className='border-1'>
+        7
+      </div>
+      <div className='border-1'>
+        8
+      </div>
+      <div className='border-1 rounded-tr-sm'>
+        9
+      </div>
+
+      {/**2 */}
+      <div className='border-1'>
+        1
+      </div>
+      <div className='border-1'>
+        2
+      </div>
+      <div className='border-1'>
+        3
+      </div>
+      <div className='border-1 col-span-2'>
+        4
+      </div>
+      <div className='border-1'>
+        6
+      </div>
+      <div className='border-1'>
+        7
+      </div>
+      <div className='border-1'>
+        8
+      </div>
+      <div className='border-1 row-span-2'>
+        9
+      </div>
+
+      {/**3 */}
+      <div className='border-1'>
+        1
+      </div>
+      <div className='border-1'>
+        2
+      </div>
+      <div className='border-1 col-span-2 row-span-2'>
+        3
+      </div>
+      <div className='border-1 col-span-3'>
+        5
+      </div>
+      <div className='border-1'>
+        8
       </div>
       
+      {/**4 */}
+      <div className='border-1'>
+        1
+      </div>
+      <div className='border-1'>
+        2
+      </div>
+      <div className='border-1'>
+        5
+      </div>
+      <div className='border-1'>
+        6
+      </div>
+      <div className='border-1'>
+        7
+      </div>
+      <div className='border-1'>
+        8
+      </div>
+      <div className='border-1'>
+        9
+      </div>
+
+      {/**5 */}
+      <div className='border-1 rounded-bl-sm row-span-2'>
+        1
+      </div>
+      <div className='border-1'>
+        2
+      </div>
+      <div className='border-1'>
+        3
+      </div>
+      <div className='border-1'>
+        4
+      </div>
+      <div className='border-1'>
+        5
+      </div>
+      <div className='border-1 col-span-3'>
+        6
+      </div>
+      <div className='border-1'>
+        9
+      </div>
+
+      {/**6 */}
+      
+      <div className='border-1'>
+        2
+      </div>
+      <div className='border-1 col-span-2'>
+        3
+      </div>
+      <div className='border-1'>
+        5
+      </div>
+      <div className='border-1'>
+        6
+      </div>
+      <div className='border-1'>
+        7
+      </div>
+      <div className='border-1'>
+        8
+      </div>
+      <div className='border-1 rounded-br-sm'>
+        9
+      </div>
     </>
   )
 }
