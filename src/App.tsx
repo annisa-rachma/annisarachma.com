0d5e9bda8abf166d41d10e40926366d34920f7c0
annisa-rachma <saa.annisa15@gmail.com>
2025-02-14 22:42:19 +0700

    add image on row 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,148 +1,151 @@
 import './App.css'
+import homeImage from './assets/home.svg'
+import githubImage from './assets/github.svg'
+import linkedinImage from './assets/linkedin.svg'
+import blogImage from './assets/blog.svg'
+import mailImage from './assets/mail.svg'
+import darkModeImage from './assets/darkmode.svg'
 
 function App() {
 
   return (
     <>
       {/**1 */}
-      <div className='border-1 rounded-tl-sm bg-purple'>
-        
+      <div className='border-[1.5px] rounded-tl-lg bg-purple grid place-items-center '>
+        <img src={homeImage} alt="" className='w-[60%]' />
       </div>
-      <div className='border-1 bg-green'>
-        
+      <div className='border-[1.5px] bg-green grid place-items-center'>
+        <img src={githubImage} alt="" className='w-[60%]' />
       </div>
-      <div className='border-1 bg-darkblue'>
-        
+      <div className='border-[1.5px] bg-darkblue grid place-items-center'>
+        <img src={linkedinImage} alt="" className='w-[55%]'/>
       </div>
-      <div className='border-1'>
-        
+      <div className='border-[1.5px]'>
       </div>
-      <div className='border-1 bg-lightblue'>
-        
+      <div className='border-[1.5px] bg-lightblue'>
       </div>
-      <div className='border-1'>
-        
+      <div className='border-[1.5px]'>
       </div>
-      <div className='border-1 bg-green'>
-        
+      <div className='border-[1.5px] bg-green grid place-items-center'>
+        <img src={blogImage} alt="" className='w-[60%]'/>
       </div>
-      <div className='border-1 bg-purple'>
-        
+      <div className='border-[1.5px] bg-purple grid place-items-center'>
+      <img src={mailImage} alt="" className='w-[60%]'/>
       </div>
-      <div className='border-1 rounded-tr-sm bg-red'>
-        
+      <div className='border-[1.5px] rounded-tr-lg bg-red grid place-items-center'>
+      <img src={darkModeImage} alt="" className='w-[55%]'/>
       </div>
 
       {/**2 */}
-      <div className='border-1'>
+      <div className='border-[1.5px]'>
         
       </div>
-      <div className='border-1 bg-red'>
+      <div className='border-[1.5px] bg-red'>
         
       </div>
-      <div className='border-1'>
+      <div className='border-[1.5px]'>
         
       </div>
-      <div className='border-1 '>
+      <div className='border-[1.5px] '>
         
       </div>
-      <div className='border-1 col-span-2 bg-yellow'>
+      <div className='border-[1.5px] col-span-2 bg-yellow'>
         
       </div>
-      <div className='border-1'>
+      <div className='border-[1.5px]'>
         
       </div>
-      <div className='border-1'>
+      <div className='border-[1.5px]'>
         
       </div>
-      <div className='border-1 row-span-2 bg-yellow'>
+      <div className='border-[1.5px] row-span-2 bg-yellow'>
         
       </div>
 
       {/**3 */}
-      <div className='border-1 bg-yellow'>
+      <div className='border-[1.5px] bg-yellow'>
         
       </div>
-      <div className='border-1'>
+      <div className='border-[1.5px]'>
         
       </div>
-      <div className='border-1 col-span-2 row-span-2 bg-lightblue'>
+      <div className='border-[1.5px] col-span-2 row-span-2 bg-lightblue'>
         
       </div>
-      <div className='border-1 col-span-3'>
+      <div className='border-[1.5px] col-span-3'>
         
       </div>
-      <div className='border-1 bg-lightblue'>
+      <div className='border-[1.5px] bg-lightblue'>
         
       </div>
       
       {/**4 */}
-      <div className='border-1'>
+      <div className='border-[1.5px]'>
         
       </div>
-      <div className='border-1 bg-purple'>
+      <div className='border-[1.5px] bg-purple'>
         
       </div>
-      <div className='border-1'>
+      <div className='border-[1.5px]'>
         
       </div>
-      <div className='border-1'>
+      <div className='border-[1.5px]'>
         
       </div>
-      <div className='border-1'>
+      <div className='border-[1.5px]'>
         
       </div>
-      <div className='border-1'>
+      <div className='border-[1.5px]'>
         
       </div>
-      <div className='border-1 bg-darkblue'>
+      <div className='border-[1.5px] bg-darkblue'>
         
       </div>
 
       {/**5 */}
-      <div className='border-1 rounded-bl-sm row-span-2 bg-lightblue'>
+      <div className='border-[1.5px] rounded-bl-lg row-span-2 bg-lightblue'>
         
       </div>
-      <div className='border-1'>
+      <div className='border-[1.5px]'>
         
       </div>
-      <div className='border-1'>
+      <div className='border-[1.5px]'>
         
       </div>
-      <div className='border-1 bg-darkblue'>
+      <div className='border-[1.5px] bg-darkblue'>
         
       </div>
-      <div className='border-1'>
+      <div className='border-[1.5px]'>
         
       </div>
-      <div className='border-1 col-span-3 bg-lightblue'>
+      <div className='border-[1.5px] col-span-3 bg-lightblue'>
         
       </div>
-      <div className='border-1'>
+      <div className='border-[1.5px]'>
         
       </div>
 
       {/**6 */}
       
-      <div className='border-1'>
+      <div className='border-[1.5px]'>
         
       </div>
-      <div className='border-1 col-span-2 bg-green'>
+      <div className='border-[1.5px] col-span-2 bg-green'>
         
       </div>
-      <div className='border-1'>
+      <div className='border-[1.5px]'>
         
       </div>
-      <div className='border-1 bg-red'>
+      <div className='border-[1.5px] bg-red'>
         
       </div>
-      <div className='border-1'>
+      <div className='border-[1.5px]'>
         
       </div>
-      <div className='border-1'>
+      <div className='border-[1.5px]'>
         
       </div>
-      <div className='border-1 rounded-br-sm bg-purple'>
+      <div className='border-[1.5px] rounded-br-lg bg-purple'>
         
       </div>
     </>
