7c84b9e94542abe59deb064b50b5066e9207f866
annisa-rachma <saa.annisa15@gmail.com>
2025-02-14 22:06:12 +0700

    add bg color to each grid

--- src/App.tsx
+++ src/App.tsx
@@ -5,145 +5,145 @@ function App() {
   return (
     <>
       {/**1 */}
-      <div className='border-1 rounded-tl-sm'>
-        1
+      <div className='border-1 rounded-tl-sm bg-purple'>
+        
+      </div>
+      <div className='border-1 bg-green'>
+        
+      </div>
+      <div className='border-1 bg-darkblue'>
+        
       </div>
       <div className='border-1'>
-        2
+        
+      </div>
+      <div className='border-1 bg-lightblue'>
+        
       </div>
       <div className='border-1'>
-        3
+        
       </div>
-      <div className='border-1'>
-        4
+      <div className='border-1 bg-green'>
+        
       </div>
-      <div className='border-1'>
-        5
+      <div className='border-1 bg-purple'>
+        
       </div>
-      <div className='border-1'>
-        6
-      </div>
-      <div className='border-1'>
-        7
-      </div>
-      <div className='border-1'>
-        8
-      </div>
-      <div className='border-1 rounded-tr-sm'>
-        9
+      <div className='border-1 rounded-tr-sm bg-red'>
+        
       </div>
 
       {/**2 */}
       <div className='border-1'>
-        1
+        
+      </div>
+      <div className='border-1 bg-red'>
+        
       </div>
       <div className='border-1'>
-        2
+        
+      </div>
+      <div className='border-1 '>
+        
+      </div>
+      <div className='border-1 col-span-2 bg-yellow'>
+        
       </div>
       <div className='border-1'>
-        3
-      </div>
-      <div className='border-1 col-span-2'>
-        4
+        
       </div>
       <div className='border-1'>
-        6
+        
       </div>
-      <div className='border-1'>
-        7
-      </div>
-      <div className='border-1'>
-        8
-      </div>
-      <div className='border-1 row-span-2'>
-        9
+      <div className='border-1 row-span-2 bg-yellow'>
+        
       </div>
 
       {/**3 */}
-      <div className='border-1'>
-        1
+      <div className='border-1 bg-yellow'>
+        
       </div>
       <div className='border-1'>
-        2
+        
       </div>
-      <div className='border-1 col-span-2 row-span-2'>
-        3
+      <div className='border-1 col-span-2 row-span-2 bg-lightblue'>
+        
       </div>
       <div className='border-1 col-span-3'>
-        5
+        
       </div>
-      <div className='border-1'>
-        8
+      <div className='border-1 bg-lightblue'>
+        
       </div>
       
       {/**4 */}
       <div className='border-1'>
-        1
+        
+      </div>
+      <div className='border-1 bg-purple'>
+        
       </div>
       <div className='border-1'>
-        2
+        
       </div>
       <div className='border-1'>
-        5
+        
       </div>
       <div className='border-1'>
-        6
+        
       </div>
       <div className='border-1'>
-        7
+        
       </div>
-      <div className='border-1'>
-        8
-      </div>
-      <div className='border-1'>
-        9
+      <div className='border-1 bg-darkblue'>
+        
       </div>
 
       {/**5 */}
-      <div className='border-1 rounded-bl-sm row-span-2'>
-        1
+      <div className='border-1 rounded-bl-sm row-span-2 bg-lightblue'>
+        
       </div>
       <div className='border-1'>
-        2
+        
       </div>
       <div className='border-1'>
-        3
+        
+      </div>
+      <div className='border-1 bg-darkblue'>
+        
       </div>
       <div className='border-1'>
-        4
+        
+      </div>
+      <div className='border-1 col-span-3 bg-lightblue'>
+        
       </div>
       <div className='border-1'>
-        5
-      </div>
-      <div className='border-1 col-span-3'>
-        6
-      </div>
-      <div className='border-1'>
-        9
+        
       </div>
 
       {/**6 */}
       
       <div className='border-1'>
-        2
+        
       </div>
-      <div className='border-1 col-span-2'>
-        3
-      </div>
-      <div className='border-1'>
-        5
+      <div className='border-1 col-span-2 bg-green'>
+        
       </div>
       <div className='border-1'>
-        6
+        
+      </div>
+      <div className='border-1 bg-red'>
+        
       </div>
       <div className='border-1'>
-        7
+        
       </div>
       <div className='border-1'>
-        8
+        
       </div>
-      <div className='border-1 rounded-br-sm'>
-        9
+      <div className='border-1 rounded-br-sm bg-purple'>
+        
       </div>
     </>
   )
