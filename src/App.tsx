2f89c16b6553d1c5acd6b7f93e4845cdc0708f62
annisa-rachma <saa.annisa15@gmail.com>
2025-03-31 04:39:31 +0700

    feat: add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 // import { useState } from 'react'
 
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      
-      <div className='bg-amber-600'>
-        aaaa
+      <div className="w-screen max-w-[100vw] p-12 flex">
+        <div className="w-[25%] border-red-100 border-2">
+          <p className="text-left">
+            🍀 <br/>
+            Hello there! My Name is Annisa. I like creating digital stuff,
+            and using the web as a creative playground. 
+            <br/>
+            In my free time, I enjoy
+            wandering around, making art, cooking comforting meals, and reading
+            books.
+          </p>
+        </div>
+        <div className="border-red-100 border-2 w-[75%]">
+          
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
