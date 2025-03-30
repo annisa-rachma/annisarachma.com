449f29748c80ec085e27924e71d774383865888b
annisa-rachma <saa.annisa15@gmail.com>
2025-03-31 05:08:04 +0700

    feat: add description

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,21 @@ function App() {
   return (
     <>
       <div className="w-screen max-w-[100vw] p-12 flex">
-        <div className="w-[25%] border-red-100 border-2">
+        <div className="w-[30%] border-red-100 border-2 font-[roboto-serif] text-2xl p-4 text-grey">
           <p className="text-left">
-            🍀 <br/>
-            Hello there! My Name is Annisa. I like creating digital stuff,
+            🍀 <br/><br/>
+            Hello there! My Name is <span className="text-black">Annisa.</span> I like creating <span className="text-black">digital stuff,</span>
             and using the web as a creative playground. 
-            <br/>
-            In my free time, I enjoy
-            wandering around, making art, cooking comforting meals, and reading
-            books.
+            <br/><br/>
+            In my free time, I enjoy 
+            <span className="text-black"> wandering</span> around, making <span className="text-black">art,</span> cooking comforting <span className="text-black"> meals,</span> and reading
+            <span className="text-black"> books.</span>
           </p>
+          <div>
+
+          </div>
         </div>
-        <div className="border-red-100 border-2 w-[75%]">
+        <div className="border-red-100 border-2 w-[70%] p-4">
           
         </div>
       </div>
