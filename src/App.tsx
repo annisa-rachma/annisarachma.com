ca1b4d47206521c6417facd7fb43c823ffd849a0
annisa-rachma <saa.annisa15@gmail.com>
2025-03-31 05:22:11 +0700

    feat: add styling to description

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,34 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="w-screen max-w-[100vw] p-12 flex">
-        <div className="w-[30%] border-red-100 border-2 font-[roboto-serif] text-2xl p-4 text-grey">
-          <p className="text-left">
-            🍀 <br/><br/>
-            Hello there! My Name is <span className="text-black">Annisa.</span> I like creating <span className="text-black">digital stuff,</span>
-            and using the web as a creative playground. 
-            <br/><br/>
-            In my free time, I enjoy 
-            <span className="text-black"> wandering</span> around, making <span className="text-black">art,</span> cooking comforting <span className="text-black"> meals,</span> and reading
+      <div className="w-screen max-w-[100vw] h-screen p-12 flex">
+        <div className="w-[30%] font-[roboto-serif] text-[1.4rem] p-6 text-grey flex flex-col justify-between text-left tracking-wide">
+          <p className="">
+            <span className="text-3xl">🍀</span> <br />
+            <br />
+            Hello there! My Name is <span className="text-black">
+              Annisa.
+            </span>{" "}
+            I like creating <span className="text-black">digital stuff,</span>
+            and using the web as a creative playground.
+            <br />
+            <br />
+            In my free time, I enjoy
+            <span className="text-black"> wandering</span> around, making{" "}
+            <span className="text-black">art,</span> cooking comforting{" "}
+            <span className="text-black"> meals,</span> and reading
             <span className="text-black"> books.</span>
           </p>
           <div>
-
+            <p>CV</p>
+            <p>Linkedin</p>
+            <p>Github</p>
+            <p>Mail</p>
           </div>
         </div>
-        <div className="border-red-100 border-2 w-[70%] p-4">
-          
+
+        <div className=" w-[70%] p-6">
+
         </div>
       </div>
     </>
