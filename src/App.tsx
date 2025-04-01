2e24864420e1ebfe19274d6014abe8f729839e2d
annisa-rachma <saa.annisa15@gmail.com>
2025-04-01 20:16:08 +0700

    fix: fixing layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,48 @@ import LinkComponent from "./components/LinkComponent";
 function App() {
   return (
     <>
-      <div className="w-screen max-w-[100vw] h-screen p-12 flex ">
-        <div className="w-[35%] font-[roboto-serif] text-[1.4rem] p-6 text-grey flex flex-col justify-between text-left tracking-wide ">
-          <p className="">
-            <span className="text-3xl">🍀</span> <br />
-            <br />
-            Hello there! My Name is <span className="text-black">
-              Annisa.
-            </span>{" "}
-            I like creating <span className="text-black">digital stuff, </span>
-            and using the web as a creative playground.
-            <br />
-            <br />
-            In my free time, I enjoy
-            <span className="text-black"> wandering</span> around, making{" "}
-            <span className="text-black">art,</span> cooking comforting{" "}
-            <span className="text-black"> meals,</span> and reading
-            <span className="text-black"> books.</span>
-          </p>
-          <div>
+      <div className="h-[100vh]  overflow-hidden relative max-w-[1350px] m-auto p-0 ">
+        <div
+          className=" static w-[100%] flex min-w-[1024px] "
+        >
+          
+          <div className="w-[35%] font-[roboto-serif] text-[1.4rem] text-grey tracking-wide absolute left-[64px] pt-[64px] block ">
+            <p className="">
+              <span className="text-3xl">🍀</span> <br />
+              <br />
+              Hello there! My Name is{" "}
+              <span className="text-black">Annisa.</span> I like creating{" "}
+              <span className="text-black">digital stuff, </span>
+              and using the web as a creative playground.
+              <br />
+              <br />
+              In my free time, I enjoy
+              <span className="text-black"> wandering</span> around, making{" "}
+              <span className="text-black">art,</span> cooking comforting{" "}
+              <span className="text-black"> meals,</span> and reading
+              <span className="text-black"> books.</span>
+            </p>
+          </div>
+          
+
+          <div className="static w-[100%] h-[100vh] overflow-y-auto no-scrollbar pt-[64px] pr-[64px] pb-[64px] pl-[550px]"
+          
+          >
+            <div className="bg-[#F6F6F6] col-span-2 rounded-2xl h-[400px]">
+              a
+            </div>
+            <div className="bg-[#F6F6F6] h-[400px] w-[400px] rounded-2xl">
+              a
+            </div>
+            <div className="bg-[#F6F6F6] h-[400px] w-[400px] rounded-2xl">
+              a
+            </div>
+            <div className="bg-[#F6F6F6] h-[400px] col-span-2 rounded-2xl">
+              a
+            </div>
+          </div>
+
+          <div className="w-[35%] font-[roboto-serif] text-[1.4rem] text-grey tracking-wide absolute left-[64px] bottom-[64px]">
             <LinkComponent title={"CV"} link="" />
             <LinkComponent
               title={"Linkedin"}
@@ -37,21 +60,6 @@ function App() {
               title={"Mail"}
               link="mailto:saa.annisa15@gmail.com"
             />
-          </div>
-        </div>
-
-        <div className=" w-[65%] p-6 grid grid-cols-2 space-x-4 space-y-4 overflow-y-auto h-screen no-scrollbar">
-          <div className="bg-[#F6F6F6] col-span-2 rounded-2xl h-[400px]">
-            a
-          </div>
-          <div className="bg-[#F6F6F6] h-[400px] w-[400px] rounded-2xl">
-            a
-          </div>
-          <div className="bg-[#F6F6F6] h-[400px] w-[400px] rounded-2xl">
-            a
-          </div>
-          <div className="bg-[#F6F6F6] h-[400px] col-span-2 rounded-2xl">
-            a
           </div>
         </div>
       </div>
