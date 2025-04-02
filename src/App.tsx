d0f409f704df0f41f63f18f7eda96c9ba47cdb71
annisa-rachma <saa.annisa15@gmail.com>
2025-04-02 23:09:31 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/projects" element={<Projects />}></Route>
         <Route path="/projects/what-to-cook" element={<ProjectDetail1 />} />
+        
       </Routes>
     </>
   );
